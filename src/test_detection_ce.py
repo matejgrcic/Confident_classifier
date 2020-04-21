b2e7ab9b0b2439d63963b999bfe066dcfc20a620
Matej Grcic <matej@lifenome.com>
2020-04-21 19:10:59 +0200

    added run_cross_entropy_ce.py

--- src/test_detection_ce.py
+++ src/test_detection_ce.py
@@ -105,7 +105,7 @@ def generate_non_target():
                 # confidence score: max_y p(y|x)
                 output = batch_output[i].view(1,-1)
                 soft_out = F.softmax(output)
-                soft_out = (1/10) * torch.log10(soft_out)
+                soft_out = (1/10) * torch.log2(soft_out)
                 f2.write("{}\n".format( - soft_out.sum().item()))
     f2.close()
 
